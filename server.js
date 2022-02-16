2adc5db3946ebfc818e0e15edfbcd44351142437
graham <gr4ham18@gmail.com>
2022-02-16 14:35:24 -0500

    style

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
 
+
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -95,6 +97,13 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
